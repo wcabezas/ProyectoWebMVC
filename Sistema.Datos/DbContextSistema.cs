917e9b402f42217a64e4906dcf55981f996d7d29
wcabezas <34169621+wcabezas@users.noreply.github.com>
2020-08-18 00:06:21 -0300

    Se termino la API REST

---
+++ Sistema.Datos/DbContextSistema.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sistema.Datos.Mapping.Almacen;
+using Sistema.Entidades.Almacen;
+
+namespace Sistema.Datos
+{
+    public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
